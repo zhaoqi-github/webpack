6b4be4d895ed8058031f95d49881089a8217838d
zhaoqi <abby.zhao@biomind.ai>
2022-09-23 15:51:58 +0800

    init

---
+++ webpack-base/src/index.js
@@ -0,0 +1,7 @@
+import generateJoke from './generateJoke';
+import './styles/main.scss';
+import laughing from './assets/laughing.svg';
+
+const laughImg = document.getElementById('laughImg');
+laughImg.src = laughing;
+console.log(generateJoke());
