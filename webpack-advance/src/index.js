2fd800126e21c7fd308e59212d78598d13654e0c
zhaoqi <abby.zhao@biomind.ai>
2022-09-26 16:22:25 +0800

    splitChunk and base learn

---
+++ webpack-advance/src/index.js
@@ -0,0 +1,9 @@
+import add from './add.js';
+import './css/index.scss';
+
+const hello = () => {
+  console.log('hello webpack');
+};
+
+hello();
+console.log(add(2, 3));
---
+++ webpack-advance/webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const resolvePath = _path => path.resolve(__dirname, _path);
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    path: resolvePath('./dist'),
+    clean: true,
+    filename: 'scripts/[name].js',
+  },
+
+  module: {
+    rules: [
+      /* {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      }, */
+      {
+        test: /\.s[ac]ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(jpe?g|png|gif|webp|svg)$/,
+        type: 'asset',
+        generator: {
+          filename: 'assets/img/[hash:10][ext]',
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolvePath('./src/index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
+  ],
+
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    open: true,
+    hot: true,
+  },
+
+  mode: 'development',
+};
