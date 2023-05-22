6ea993d8ac92910f7721db0a2a803432749d961c
zhaoqi <abby.zhao@biomind.ai>
2023-05-22 13:36:39 +0800

    feat: 多入口打包

--- ZBestPC-update/webpack.config.js
+++ ZBestPC-update/webpack.config.js
@@ -5,11 +5,19 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   mode: 'development',
   entry: {
-    bundle: './src/index.js'
+    index: './src/index.js',
+    login: './src/login.js'
   },
   output: {
     filename: 'js/[name].js',
     path: path.resolve(__dirname, './dist')
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist')
+    },
+    compress: true, // 压缩
+    hot: true
   },
   module: {
     rules: [
@@ -36,11 +44,13 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: './src/index.html'
+      template: './src/index.html',
+      chunks: ['index']
     }),
     new HtmlWebpackPlugin({
       filename: 'login.html',
-      template: './src/login.html'
+      template: './src/login.html',
+      chunks: ['login']
     }),
     new webpack.ProvidePlugin({
       $: 'jquery',
