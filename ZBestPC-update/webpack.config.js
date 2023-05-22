cdfe34c0f5e908a719915dabb90bc74621c90f0f
zhaoqi <abby.zhao@biomind.ai>
2023-05-22 13:53:33 +0800

    feat: 项目开发模式+ CopyWebpackPlugin自动拷贝

--- ZBestPC-update/webpack.config.js
+++ ZBestPC-update/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -55,6 +56,14 @@ module.exports = {
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, './src/img'),
+          to: path.resolve(__dirname, './dist/img')
+        }
+      ]
     })
   ]
 }
