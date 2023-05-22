06ee377ee0b1703c9c32815944b019b6e78247f7
zhaoqi <abby.zhao@biomind.ai>
2023-05-22 14:52:44 +0800

    feat: treeshaking

--- ZBestPC-update/src/index.js
+++ ZBestPC-update/src/index.js
@@ -4,3 +4,17 @@ import './css/index.css'
 import 'jquery'
 import './js/public'
 import './js/nav'
+
+// 1. 通过解构获取方法，可以触发 treeshaking 
+// 2. 调用的 npm 包必须满足 ES module 
+/* export function a() { } // 这种才行
+export default { // 这种不行
+  a() { },
+  b() { }
+} */
+// import { get } from 'lodash'
+
+/* import { get } from 'lodash-es'
+console.log(get({ a: 1 }, 'a')) */
+
+// 3. 同一文件下 treeshaking 触发条件是 mode=production
