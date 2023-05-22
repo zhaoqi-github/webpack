41b501f6a39563e7e270c9aac92d0d1e64a0073e
zhaoqi <abby.zhao@biomind.ai>
2023-05-22 16:47:23 +0800

    feat: Vue 构建前的准备工作

---
+++ ZBestPC-update/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">{{ message }}</div>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  data() {
+    return {
+      message: 'hello test'
+    }
+  }
+})
+</script>
+<style>
+</style>
